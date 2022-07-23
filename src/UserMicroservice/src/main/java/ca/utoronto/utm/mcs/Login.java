241c3af13df71c40048dc2efdc178315108325f9
Theodliu <theodtheodliu@gmail.com>
2022-07-23 15:15:24 -0400

    upload user-login-api
    upload user-login-api

--- src/UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Login.java
+++ src/UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Login.java
@@ -14,8 +14,50 @@ public class Login extends Endpoint {
      * information of the user in the database.
      */
     
+    public int user_login(JSONObject deserialized){
+        String email, password;
+        int user_login_res;
+        try{
+            if(deserialized.has("email") && deserialized.has("password")){
+                //check whether the user exists
+                email = deserialized.getString("email");
+                password = deserialized.getString("password");
+            } else{
+                return 400;
+            }
+        } catch(Exception e){
+            //can't find the string from deserailized
+            e.printStackTrace();
+            return 500;
+        }
+        try{
+            user_login_res = this.dao.user_login(email, password);
+        } catch (Exception e){
+            e.printStackTrace();
+            return 500;
+        }
+        return user_login_res
+    }
+
     @Override
     public void handlePost(HttpExchange r) throws IOException, JSONException {
-        // TODO        
+        // TODO
+        String body = Utils.convert(r.getRequestBody());
+        String path = r.getRequestURI().getPath();
+        int api_response;
+        try{
+            switch(path){
+                //distinguish the path
+                case "/api/user/login":
+                    api_response = this.user_login();
+                    sendStatus(r,api_response);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
     }
+
+
+
 }
