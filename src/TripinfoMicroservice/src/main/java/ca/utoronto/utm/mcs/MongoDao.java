f010a386b4bfef5677b3f9c1b772bcf022b86e74
PitcairnHotdog <pitcairnzhang@outlook.com>
2022-07-25 18:55:39 -0400

    Implemented trip confirm db

--- src/TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
+++ src/TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
@@ -1,6 +1,9 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
 
 public class MongoDao {
@@ -11,8 +14,21 @@ public class MongoDao {
         // TODO: 
         // Connect to the mongodb database and create the database and collection. 
         // Use Dotenv like in the DAOs of the other microservices.
+		Dotenv dotenv = Dotenv.load();
+		String addr = dotenv.get("MONGODB_ADDR");
+		MongoClient mongoClient = new MongoClient("mongodb://");
+		MongoDatabase mongoDatabase = mongoClient.getDatabase("trip");
+		this.collection = mongoDatabase.getCollection("trips");
 	}
 
 	// *** implement database operations here *** //
+
+	public void trip_confirm(String driverID, String passengerID, int startTime) {
+		Document document = new Document();
+		document.append("driver", driverID);
+		document.append("passenger", passengerID);
+		document.append("startTime", startTime);
+		collection.insertOne(document);
+	}
 
 }
