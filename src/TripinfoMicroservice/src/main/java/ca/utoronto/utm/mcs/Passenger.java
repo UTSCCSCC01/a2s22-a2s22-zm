7cf4a142c2ebd28bc8788ab5e45f5520cc3cf704
Theodliu <theodtheodliu@gmail.com>
2022-07-25 20:01:31 -0400

    upload trip-passenger-api
    upload trip-passenger-api

--- src/TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Passenger.java
+++ src/TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Passenger.java
@@ -2,6 +2,8 @@ package ca.utoronto.utm.mcs;
 
 import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class Passenger extends Endpoint {
@@ -15,6 +17,30 @@ public class Passenger extends Endpoint {
 
     @Override
     public void handleGet(HttpExchange r) throws IOException,JSONException{
-        // TODO
+        String params[] = r.getRequestURI().toString().split("/");
+        if (params.length != 4 || params[3].isEmpty()) {
+            this.sendStatus(r, 400);
+            return;
+        }
+        String uid = params[3];
+        JSONObject body = new JSONObject(Utils.convert(r.getRequestBody()));
+        if(body.length()!=0){
+            this.sendStatus(r, 400);
+            return;
+        }
+        try {
+            JSONObject new_data;
+            JSONObject res = new JSONObject();
+            new_data = this.dao.trip_passenger(uid);
+            if(new_data.has("trips")){
+                res.put("data", new_data);
+                this.sendResponse(r,res,200);
+            }else{
+                this.sendStatus(r,404);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            this.sendStatus(r,500);
+        }
     }
 }
