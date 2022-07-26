a0c315e5041477bcad42e014fea95d450a737a12
Theodliu <theodtheodliu@gmail.com>
2022-07-25 21:01:17 -0400

    upload trip-drivertime-api
    upload trip-drivertime-api

--- src/TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Drivetime.java
+++ src/TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Drivetime.java
@@ -6,6 +6,7 @@ package ca.utoronto.utm.mcs;
  * and/or recieve http requests from other microservices. Any other 
  * imports are fine.
  */
+import java.io.OutputStream;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -13,9 +14,15 @@ import java.net.URI;
 
 import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
+import static ca.utoronto.utm.mcs.Request.httpRequest;
+
 public class Drivetime extends Endpoint {
+
+    static final String LOCATION_SERVICE = "src_locationmicroservice";
 
     /**
      * GET /trip/driverTime/:_id
@@ -28,6 +35,36 @@ public class Drivetime extends Endpoint {
 
     @Override
     public void handleGet(HttpExchange r) throws IOException, JSONException {
-        // TODO
+        String params[] = r.getRequestURI().toString().split("/");
+        if (params.length != 4 || params[3].isEmpty()) {
+            this.sendStatus(r, 400);
+            return;
+        }
+        String tid = params[3];
+        JSONObject body = new JSONObject(Utils.convert(r.getRequestBody()));
+        if(body.length()!=0){
+            this.sendStatus(r, 400);
+            return;
+        }
+        try{
+            JSONObject new_data = this.dao.trip_drivertime(tid);
+            if(!new_data.has("driver")){
+                this.sendStatus(r,404);
+                return;
+            }
+            String driveruid = new_data.getString("driver");
+            String passengeruid = new_data.getString("passenger")
+            HttpResponse<String> httpResponse = httpRequest("GET", LOCATION_SERVICE, "/location/navigation/" + driveruid + "?passengerUid=" + passengeruid, "");
+            JSONObject content = new JSONObject(httpResponse.body());
+            int time = content.getJSONObject("data").getInt("total_time");
+            JSONObject data = new JSONObject();
+            data.put("arrival_time", time);
+            JSONObject res = new JSONObject();
+            res.put("data", data);
+            sendResponse(r,res,200);
+        } catch (Exception e){
+            e.printStackTrace();
+            this.sendStatus(r,500);
+        }
     }
 }
